5da7069fff5a83d8a684532dd01be56997dda34f
ganovelli <fabio.ganovelli@gmail.com>
2024-06-12 18:18:53 +0200

    bugfix
    wrong filtering and mimap target

--- src/common/texture.h
+++ src/common/texture.h
@@ -73,8 +73,8 @@ struct texture {
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glGenerateMipmap(GL_TEXTURE_2D);	
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
 		for (unsigned int i = 0; i < 6; ++i)
 			stbi_image_free(data[i]);
 		return id;
